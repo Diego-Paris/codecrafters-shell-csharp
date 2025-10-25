dc942212bd218d75228fcadb774eb81622c892a0
diego-paris <diego.paris@upr.edu>
2025-10-24 18:33:43 -0700

    test

--- src/main.cs
+++ src/main.cs
@@ -4,5 +4,8 @@ class Program
     {
         // TODO: Uncomment the code below to pass the first stage
         Console.Write("$ ");
+        var input = Console.ReadLine();
+        
+        Console.WriteLine($"{input}: command not found");
     }
 }
