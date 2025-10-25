2a7234daf5c2159272bdf44715e093a938b830d3
diego-paris <diego.paris@upr.edu>
2025-10-24 18:38:00 -0700

    add repl behavior

--- src/main.cs
+++ src/main.cs
@@ -2,10 +2,12 @@ class Program
 {
     static void Main()
     {
-        // TODO: Uncomment the code below to pass the first stage
-        Console.Write("$ ");
-        var input = Console.ReadLine();
-        
-        Console.WriteLine($"{input}: command not found");
+        while (true)
+        {
+            Console.Write("$ ");
+            var input = Console.ReadLine();
+
+            Console.WriteLine($"{input}: command not found");
+        }
     }
 }
