7e48f7313952d5d97d9d421de69acfbccf9a2665
diego-paris <diego.paris@upr.edu>
2025-10-24 22:05:26 -0700

    add type

--- src/main.cs
+++ src/main.cs
@@ -1,17 +1,22 @@
-using System.Text;
-
 class Program
 {
     static int Main()
     {
-        var commands = new Dictionary<string, Action<string[]>>
+        var commands = new Dictionary<string, Action<string[]>>();
+
+        commands["greet"] = args => Console.WriteLine("Hello, World!");
+
+        commands["exit"] = args =>
         {
-            { "greet", args => Console.WriteLine("Hello, World!") },
-            { "exit", args => {
-                var exitCode = args.Length > 0 && int.TryParse(args[0], out var code) ? code : 0;
-                Environment.Exit(exitCode);
-            }},
-            { "echo", words => Console.WriteLine(string.Join(' ', words)) },
+            var exitCode = args.Length > 0 && int.TryParse(args[0], out var code) ? code : 0;
+            Environment.Exit(exitCode);
+        };
+
+        commands["echo"] = words => Console.WriteLine(string.Join(' ', words));
+
+        commands["type"] = args =>
+        {
+            Console.WriteLine(commands.ContainsKey(args[0]) && args.Length == 1 ? $"{args[0]} is a builtin" : $"{args[0]}: not found");
         };
 
         while (true)
