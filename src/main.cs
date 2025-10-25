df28b3438e053211067bc75633f076e7e01cf65e
diego-paris <diego.paris@upr.edu>
2025-10-24 20:15:06 -0700

    add echo

--- src/main.cs
+++ src/main.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 class Program
 {
     static int Main()
@@ -8,7 +10,15 @@ class Program
             { "exit", args => {
                 var exitCode = args.Length > 0 && int.TryParse(args[0], out var code) ? code : 0;
                 Environment.Exit(exitCode);
-            }}
+            }},
+            { "echo", words => {
+                var message = new StringBuilder();
+                foreach (var word in words)
+                {
+                    message.Append(word).Append(' ');
+                }
+                Console.WriteLine(message);
+            }},
         };
 
         while (true)
@@ -16,7 +26,7 @@ class Program
             Console.Write("$ ");
             var input = Console.ReadLine();
             var parts = input?.Split(' ', StringSplitOptions.RemoveEmptyEntries) ?? Array.Empty<string>();
-            
+
             if (parts.Length > 0 && commands.TryGetValue(parts[0], out var command))
             {
                 command(parts.Skip(1).ToArray());
