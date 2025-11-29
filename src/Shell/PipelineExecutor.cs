1b91ea7194f25e6de2be2bb6c6759a8af8d9042a
diego-paris <diego.paris@upr.edu>
2025-11-28 20:32:18 -0800

    add builtin support in pipelines

--- src/Shell/PipelineExecutor.cs
+++ src/Shell/PipelineExecutor.cs
@@ -1,5 +1,7 @@
 using MiniShell.Abstractions;
+using MiniShell.Runtime;
 using System.Diagnostics;
+using System.Text;
 
 namespace MiniShell;
 
@@ -17,7 +19,9 @@ public class PipelineExecutor
         if (segments.Count == 0) return 0;
         if (segments.Count == 1) return 0;
 
+        var buffers = new List<StringWriter?>();
         var processes = new List<Process>();
+        var tasks = new List<Task<int>>();
 
         try
         {
@@ -29,52 +33,131 @@ public class PipelineExecutor
                 var commandName = segment[0];
                 var args = segment.Skip(1).ToArray();
 
-                var resolvedPath = _ctx.PathResolver.FindInPath(commandName);
-                if (resolvedPath == null)
-                {
-                    _ctx.Err.WriteLine($"{commandName}: command not found");
-                    CleanupProcesses(processes);
-                    return 127;
-                }
+                var isBuiltin = _ctx.Commands.ContainsKey(commandName);
 
-                var process = new Process();
-                process.StartInfo.FileName = resolvedPath;
-                process.StartInfo.Arguments = string.Join(' ', args.Select(QuoteIfNeeded));
-                process.StartInfo.UseShellExecute = false;
-                process.StartInfo.CreateNoWindow = true;
-
-                if (i == 0)
+                if (isBuiltin)
                 {
-                    process.StartInfo.RedirectStandardInput = false;
+                    TextReader inputReader;
+                    if (i > 0 && buffers[i - 1] != null)
+                    {
+                        var previousOutput = buffers[i - 1]!.ToString();
+                        inputReader = new StringReader(previousOutput);
+                    }
+                    else
+                    {
+                        inputReader = _ctx.In;
+                    }
+
+                    TextWriter outputWriter;
+                    StringWriter? buffer = null;
+
+                    if (i < segments.Count - 1)
+                    {
+                        buffer = new StringWriter();
+                        outputWriter = buffer;
+                        buffers.Add(buffer);
+                    }
+                    else
+                    {
+                        outputWriter = _ctx.Out;
+                    }
+
+                    var errorWriter = _ctx.Err;
+
+                    var builtinContext = new ShellContext(
+                        _ctx.Commands.Values,
+                        _ctx.PathResolver,
+                        inputReader,
+                        outputWriter,
+                        errorWriter);
+
+                    var localInput = inputReader;
+                    var localOutput = outputWriter;
+                    var task = Task.Run(() =>
+                    {
+                        try
+                        {
+                            var cmd = _ctx.Commands[commandName];
+                            var exitCode = cmd.Execute(args, builtinContext);
+                            localOutput.Flush();
+                            return exitCode;
+                        }
+                        catch
+                        {
+                            return 1;
+                        }
+                        finally
+                        {
+                            if (localInput != _ctx.In)
+                            {
+                                localInput.Dispose();
+                            }
+                        }
+                    });
+
+                    tasks.Add(task);
                 }
                 else
                 {
-                    process.StartInfo.RedirectStandardInput = true;
-                }
+                    var resolvedPath = _ctx.PathResolver.FindInPath(commandName);
+                    if (resolvedPath == null)
+                    {
+                        _ctx.Err.WriteLine($"{commandName}: command not found");
+                        CleanupAll(processes, buffers);
+                        return 127;
+                    }
 
-                if (i == segments.Count - 1)
-                {
-                    process.StartInfo.RedirectStandardOutput = false;
-                    process.StartInfo.RedirectStandardError = false;
-                }
-                else
-                {
-                    process.StartInfo.RedirectStandardOutput = true;
-                    process.StartInfo.RedirectStandardError = true;
-                }
+                    var process = new Process();
+                    process.StartInfo.FileName = resolvedPath;
+                    process.StartInfo.Arguments = string.Join(' ', args.Select(QuoteIfNeeded));
+                    process.StartInfo.UseShellExecute = false;
+                    process.StartInfo.CreateNoWindow = true;
 
-                process.Start();
-                processes.Add(process);
+                    if (i == 0)
+                    {
+                        process.StartInfo.RedirectStandardInput = false;
+                    }
+                    else
+                    {
+                        process.StartInfo.RedirectStandardInput = true;
+                    }
 
-                if (i > 0)
-                {
-                    var previousProcess = processes[i - 1];
-                    CopyStreamAsync(previousProcess.StandardOutput, process.StandardInput);
+                    if (i == segments.Count - 1)
+                    {
+                        process.StartInfo.RedirectStandardOutput = false;
+                        process.StartInfo.RedirectStandardError = false;
+                    }
+                    else
+                    {
+                        process.StartInfo.RedirectStandardOutput = true;
+                        process.StartInfo.RedirectStandardError = true;
+                    }
+
+                    process.Start();
+                    processes.Add(process);
+
+                    if (i > 0)
+                    {
+                        if (buffers.Count > 0 && buffers[i - 1] != null)
+                        {
+                            var previousOutput = buffers[i - 1]!.ToString();
+                            WriteToProcessInputAsync(previousOutput, process.StandardInput);
+                        }
+                        else if (processes.Count > 1)
+                        {
+                            var previousProcess = processes[processes.Count - 2];
+                            CopyStreamAsync(previousProcess.StandardOutput, process.StandardInput);
+                        }
+                    }
+
+                    if (i < segments.Count - 1)
+                    {
+                        buffers.Add(null);
+                    }
                 }
             }
 
-            var lastProcess = processes[processes.Count - 1];
-            lastProcess.WaitForExit();
+            Task.WaitAll(tasks.ToArray());
 
             foreach (var p in processes)
             {
@@ -84,12 +167,22 @@ public class PipelineExecutor
                 }
             }
 
-            return lastProcess.ExitCode;
+            if (tasks.Count > 0)
+            {
+                return tasks[tasks.Count - 1].Result;
+            }
+
+            if (processes.Count > 0)
+            {
+                return processes[processes.Count - 1].ExitCode;
+            }
+
+            return 0;
         }
         catch (Exception ex)
         {
             _ctx.Err.WriteLine($"Pipeline error: {ex.Message}");
-            CleanupProcesses(processes);
+            CleanupAll(processes, buffers);
             return 1;
         }
         finally
@@ -98,6 +191,24 @@ public class PipelineExecutor
             {
                 p.Dispose();
             }
+
+            foreach (var buffer in buffers)
+            {
+                buffer?.Dispose();
+            }
+        }
+    }
+
+    private static async void WriteToProcessInputAsync(string content, StreamWriter destination)
+    {
+        try
+        {
+            await destination.WriteAsync(content);
+            destination.Close();
+        }
+        catch
+        {
+            // Ignore pipe errors
         }
     }
 
@@ -114,7 +225,7 @@ public class PipelineExecutor
         }
     }
 
-    private static void CleanupProcesses(List<Process> processes)
+    private static void CleanupAll(List<Process> processes, List<StringWriter?> buffers)
     {
         foreach (var p in processes)
         {
@@ -124,6 +235,18 @@ public class PipelineExecutor
                 {
                     p.Kill();
                 }
+            }
+            catch
+            {
+                // Ignore
+            }
+        }
+
+        foreach (var buffer in buffers)
+        {
+            try
+            {
+                buffer?.Dispose();
             }
             catch
             {
